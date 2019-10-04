8c17ab918d21b56e15d482e50f7c4e713786db87
Noah Gilmore <noah.gilmore@autodesk.com>
2019-10-04 15:31:50 -0700

    Set up example

---
+++ example.swift
@@ -0,0 +1,21 @@
+struct TakesAClosure {
+    let closure: () -> Void
+}
+
+struct Example {
+    let one: String
+    let two: String
+    let closureStructs: [String: TakesAClosure]
+    let four: String
+}
+
+let example = Example(one: "Hey"
+                            two: "Hey"
+                            closureStructs: [
+                            "A": TakesAClosure(closure: {
+                                print("A")
+                            }),
+                            "B": TakesAClosure(closure: {
+
+                            })],
+                            four: "Hey")
